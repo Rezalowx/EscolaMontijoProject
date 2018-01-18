5d3f544e77423ff7a33776945ed9b68118870aa9
Rezalowx <gregory.brugnet@gmail.com>
2018-01-18 10:22:06 +0100

    17.01 commit without fail

--- EscolaProMontijo/ConnectionMySql.cs
+++ EscolaProMontijo/ConnectionMySql.cs
@@ -3,6 +3,7 @@ using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 using System.Data;
 
+
 namespace EscolaProMontijo
 {
     class connectionMySql
@@ -10,36 +11,44 @@ namespace EscolaProMontijo
         private string MyconnectionString = "Server=127.0.0.1;Database=bddmontijotest;Uid=root;Pwd=";
 
         /// <summary>
-        /// 
+        /// Connect to the database
         /// </summary>
         /// <returns></returns>
         public MySqlConnection ConnectionMySql()
         {
+           
             MySqlConnection connection = new MySqlConnection(MyconnectionString);
             return connection;
+            
         }
         /// <summary>
-        /// 
+        /// Disconnect sql
         /// </summary>
-        /// <param name="ConnectionInfo"></param>
+        /// <param name="ConnectionInfo">a valid MySqlConnection</param>
         public void DisconnectMySql(MySqlConnection ConnectionInfo)
         {
             ConnectionInfo.Close();
         }
 
         /// <summary>
-        /// 
+        /// return the connection string
         /// </summary>
-        /// <param name="selectCommand"></param>
-        /// <param name="dgv"></param>
-        /// <param name="bindingSource"></param>
+        /// <returns></returns>
+        public string getMyconnectionString()
+        {
+            return MyconnectionString;
+        }
+       
+        /// <summary>
+        /// Get data of sql query in a datagridview
+        /// </summary>
+        /// <param name="selectCommand">Sql query</param>
+        /// <param name="dgv">Datagridview you want your data in</param>
+        /// <param name="bindingSource">Binding source of the datagridview</param>
         public void getData(string selectCommand, DataGridView dgv, BindingSource bindingSource) // Get et update les datas dans une dgv
         {
             try
             {
-               
-               
-
                 // Create a new data adapter based on the specified query.
                 var dataAdapter = new MySqlDataAdapter(selectCommand, MyconnectionString);
 
@@ -66,9 +75,9 @@ namespace EscolaProMontijo
         }
 
         /// <summary>
-        /// 
+        /// Get a string[] which contains the infos of the company
         /// </summary>
-        /// <param name="querysql"></param>
+        /// <param name="nameCompany">The name of the company in the database</param>
         /// <returns></returns>
         public string[] getInfosinStringArrayOfCompany (string nameCompany)
         {
@@ -91,13 +100,80 @@ namespace EscolaProMontijo
             }
 
         }
+        public string[] getInfosinStringArrayOfCompany(string nameCompany, string emailCompany)
+        {
+            MySqlConnection con = ConnectionMySql();
+            con.Open();
+
+            string[] row = new string[4];
+            string querysql = "SELECT c.name, c.email, c.numero, c.address FROM company c where name ='" + nameCompany +"' AND c.email ='"+emailCompany+"'";
+            using (var command = new MySqlCommand(querysql, con))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    //Iterate through the rows and add it to the combobox's items
+                    while (reader.Read())
+                    {
+                        row = new string[] { reader.GetString("name"), reader.GetString("email"), reader.GetString("numero"), reader.GetString("address") };
+                    }
+                    return row;
+                }
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sectorName"></param>
+        /// <returns></returns>
+        public string getIdSectorFromName(string sectorName)
+        {
+            MySqlConnection con = ConnectionMySql();
+            con.Open();
+            string sqlQuery = "SELECT id FROM sector WHERE name ='" + sectorName + "'";
+
+            using (var command = new MySqlCommand(sqlQuery, con))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    reader.Read();
+                    return reader.GetString("id");
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="idSector"></param>
+        /// <param name="name"></param>
+        /// <param name="email"></param>
+        /// <param name="numero"></param>
+        /// <param name="address"></param>
+        public void addNewCompany(string idSector, string name, string email, string numero, string address) // FIX THIS
+        {
+            MySqlConnection con = ConnectionMySql();
+            con.Open();
+            
+            string sqlQuery = "INSERT INTO `company`( idSector, `name`, `email`, `Numero`, `address`) VALUES ( '"+idSector+"' , '" + name + "' , '" + email + "', '" + numero + "', '" + address + "')";
+
+            var command = new MySqlCommand(sqlQuery, con);
+            command.ExecuteNonQuery();
+            
+        }
+        public void deleteCompany(string name, string email)
+        {
+            MySqlConnection con = ConnectionMySql();
+            con.Open();
+
+            string sqlQuery = "DELETE FROM company WHERE name ='" + name + "' AND email ='"+email+"'";
+        }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="querySql"></param>
-        /// <param name="combobox"></param>
-        /// <param name="column"></param>
+        /// <param name="querySql">a query sql</param>
+        /// <param name="combobox">the combobox you want the result of the query in</param>
+        /// <param name="column"column of the table you want in your combobox</param>
         public void PutQueryIntoComboBox(string querySql, ComboBox combobox, string column)
         {
             MySqlConnection con = ConnectionMySql();
@@ -119,6 +195,13 @@ namespace EscolaProMontijo
                 }
             }
         }
+
+        /// <summary>
+        /// Add a string[] into the last row of a datagridview
+        /// </summary>
+        /// <param name="dgv">datagridview you work with</param>
+        /// <param name="bs">binding source of the datagridview</param>
+        /// <param name="array">valid array you want to add</param>
 
         public void PutQueryIntoLastRow(DataGridView dgv, BindingSource bs, string[] array)
         {
@@ -143,6 +226,43 @@ namespace EscolaProMontijo
                 DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
 
         }
+
+        /// <summary>
+        /// Delete the company you want in the datagridview
+        /// </summary>
+        /// <param name="companyToDelete">Company you want to delete</param>
+        /// <param name="dgv">Datagridview you work with</param>
+        public void DeleteCompanyInDatagridView (string companyToDelete, DataGridView dgv)
+        {
+            for (int i = 0; i < dgv.Rows.Count - 1; i++) 
+            {
+                string companyToCheck = dgv.Rows[i].Cells[0].Value.ToString();
+                if (companyToCheck == companyToDelete)
+                {
+                    dgv.Rows.Remove(dgv.Rows[i]);
+                }
+            }
+        }
+        public void DeleteCompanyInDatagridView(string companyToDelete, string emailToDelete, DataGridView dgv)
+        {
+            for (int i = 0; i < dgv.Rows.Count - 1; i++)
+            {
+                string companyToCheck = dgv.Rows[i].Cells[0].Value.ToString();
+                string emailToCheck = dgv.Rows[i].Cells[1].Value.ToString();
+                if (companyToCheck == companyToDelete && emailToCheck == emailToDelete)
+                {
+                    dgv.Rows.Remove(dgv.Rows[i]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add a new company in database
+        /// </summary>
+        /// <param name="name">Company's name</param>
+        /// <param name="email">Company's email</param>
+        /// <param name="numero">Company's numero</param>
+        /// <param name="address">Company's address</param>
 
 
     }
