a9d8ccc42994760061932e0f6493bb490b41cc17
Rezalowx <gregory.brugnet@gmail.com>
2018-02-07 18:20:08 +0100

    Commit 07.02.2018

--- EscolaProMontijo/SendMails.cs
+++ EscolaProMontijo/SendMails.cs
@@ -20,7 +20,6 @@ namespace EscolaProMontijo
         /// <param name="emailTo">string email to</param>
         /// <param name="subject">string email's subject</param>
         /// <param name="attachment">List<string> of attachments (paths) </param>
-
         public void sendAMail(string message, string emailFrom, string emailTo, string subject, List<string> attachment)
         {
 
@@ -48,7 +47,7 @@ namespace EscolaProMontijo
             MyMsg.From = new MailAddress("gregory.brugnet@gmail.com","TCHOUPINOU'S APPLICATION FAIT DES TESTS SUR TWA");
             MyMsg.BodyEncoding = Encoding.UTF8;
             MyMsg.Body = message;
-            client.Credentials = new System.Net.NetworkCredential("gregory.brugnet@gmail.com", "passwordLOL");
+            client.Credentials = new System.Net.NetworkCredential("gregory.brugnet@gmail.com", "xDkikoolol1");
 
             
             client.Send(MyMsg);
@@ -56,7 +55,49 @@ namespace EscolaProMontijo
 
         }
 
-       
-        
+        /// <summary>
+        /// Send an email
+        /// </summary>
+        /// <param name="message">string message</param>
+        /// <param name="emailFrom">string email from</param>
+        /// <param name="emailTo">string email to</param>
+        /// <param name="subject">string email's subject</param>
+        /// <param name="signature">string signature</param>
+        /// <param name="attachment">List<string> of attachments (paths) </param>
+        public void sendAMail(string message, string emailFrom, string emailTo, string subject, string signature, List<string> attachment)
+        {
+
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            MailMessage MyMsg = new MailMessage();
+            MyMsg.Priority = MailPriority.High;
+            MyMsg.To.Add(new MailAddress(emailTo));
+            client.EnableSsl = true;
+            if (attachment[0] != "")
+            {
+
+                foreach (string attachmentsInList in attachment)
+                {
+
+                    MyMsg.Attachments.Add(new Attachment(attachmentsInList));
+                }
+
+            }
+            MyMsg.Subject = subject;
+            MyMsg.SubjectEncoding = Encoding.UTF8;
+            MyMsg.IsBodyHtml = true;
+            MyMsg.From = new MailAddress("gregory.brugnet@gmail.com", "TCHOUPINOU'S APPLICATION FAIT DES TESTS SUR TWA");
+            MyMsg.BodyEncoding = Encoding.UTF8;
+            MyMsg.Body = message+ "<br />" + signature;
+            client.Credentials = new System.Net.NetworkCredential("gregory.brugnet@gmail.com", "PASSWORDLOL");
+
+
+            client.Send(MyMsg);
+
+
+        }
+
     }
 }
